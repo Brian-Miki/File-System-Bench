474605a130617d8a3b37e306f65b3c64eabef196
Brian Miki <bmiki5508@gmail.com>
2026-01-11 14:38:59 -0800

    chore: refactor code and organize files

---
+++ providers/data_loader.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import json
+
+
+def get_news() -> str:
+    articles = ""
+
+    folder = Path("data/games")
+    for file_path in folder.iterdir():
+        with file_path.open("r", encoding="utf-8") as f:
+            contents = f.read()
+            articles += contents
+    return articles
+
+
+def get_questions() -> list[str]:
+    questions = []
+    with open("data/tests/tests.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+        for question in data["questions_and_answers"]:
+            questions.append(question["question"])
+    return questions
--- providers/model.py
+++ providers/model.py
@@ -1,30 +1,10 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
-from pathlib import Path
-import json
+from data_loader import get_questions, get_news
+
 
 load_dotenv()
-
-
-def get_news() -> str:
-    articles = ""
-
-    folder = Path("data/games")
-    for file_path in folder.iterdir():
-        with file_path.open("r", encoding="utf-8") as f:
-            contents = f.read()
-            articles += contents
-    return articles
-
-
-def get_questions() -> list[str]:
-    questions = []
-    with open("data/tests/tests.json", "r", encoding="utf-8") as f:
-        data = json.load(f)
-        for question in data["questions_and_answers"]:
-            questions.append(question["question"])
-    return questions
 
 
 def openai_call() -> list[str]:
@@ -40,3 +20,6 @@ def openai_call() -> list[str]:
         print(response.output_text)
         output.append(response.output_text)
     return output
+
+
+openai_call()
