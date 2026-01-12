d08da16399acfd553047d60d588e75d723b28219
Brian Miki <bmiki5508@gmail.com>
2026-01-11 18:07:33 -0800

    chore: removed unused imports

--- providers/openai_client.py
+++ providers/openai_client.py
@@ -1,13 +1,5 @@
 from openai import OpenAI
 import os
-from dotenv import load_dotenv
-from data_loader import get_questions, get_news, get_summaries
-from tools import grep_file, tool_description
-import json
-
-
-
-
 
 def get_openai_client():
     api_key = os.getenv("OPENAI_API_KEY")
